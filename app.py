3c5a89ec39b99be94d14e34861a2f70cc080dfe6
Rachit <rachit@Rachits-MacBook-Air.local>
2023-06-10 16:11:38 +0530

    This is final completion of project with web deployment Using floask

---
+++ app.py
@@ -0,0 +1,56 @@
+# import All the necessary python packages
+from flask import Flask, render_template, request
+import joblib
+import os
+import numpy as np
+import pickle as pk
+
+# Initiate the app
+app = Flask(__name__)
+# Take the first request and render the home page
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+# Render the submit request
+@app.route("/result", methods=['POST', 'GET'])
+def result():
+    # Collect the information from the form
+    gender = int(request.form['gender'])
+    age = int(request.form['age'])
+    hypertension = int(request.form['hypertension'])
+    heart_disease = int(request.form['heart_disease'])
+    ever_married = int(request.form['ever_married'])
+    work_type = int(request.form['work_type'])
+    Residence_type = int(request.form['Residence_type'])
+    avg_glucose_level = float(request.form['avg_glucose_level'])
+    bmi = float(request.form['bmi'])
+    smoking_status = int(request.form['smoking_status'])
+
+    # Convert the information to the numpy array
+    x = np.array([gender, age, hypertension, heart_disease, ever_married, work_type,
+                 Residence_type, avg_glucose_level, bmi, smoking_status]).reshape(1, -1)
+
+    # Load the classfier to transform the input into a standard or normalized form
+    with open('model/scaler.pkl', 'rb') as f:
+        scaler = pk.load(f)
+    x = scaler.transform(x)
+
+    # Load the model to predict the results
+    with open('model/lr.sav', 'rb') as f:
+        model = pk.load(f)
+    Y_pred = model.predict(x)
+
+    # render the page based on the value of model prediction
+    if Y_pred == 0:
+        return render_template('nostroke.html')
+    else:
+        return render_template('stroke.html')
+
+
+# basic template to run the application
+if __name__ == "__main__":
+    app.run(debug=True, port=7384)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Here we are predicting wether a user has chances of Brain stroke or not based on the famous AI algorithms.
+<br/>
+## We are using logistic regression,KNN,SVM,decision Tree and random forest algorithms.
+<hr/>
+<p>You can find the <a href = "https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset">dataset</a> in the kaggle here </p>
+<hr/>
+
+<p>We loaded the LogisticRegression model for final prediction but we can use any of the model above.</p><br/>
+
+<p>To predict the results just execute the following command</p><br/>
+`python3 app.py`
+<br/><p>You will get a link to access the web page</p>
